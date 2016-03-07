46958da584801ef082b8ed6bac9196c0682b0728
awatson32 <audrey.watson32@gmail.com>
2016-03-07 11:20:27 -0600

    stuff

---
+++ classes.js
@@ -0,0 +1,45 @@
+var Die = function() {
+    this.value = null;
+    this.div = null;   
+    this.insert = function() {
+        this.div = document.createElement('div');
+        this.div.className = 'die';
+        this.roll();
+        document.getElementById('dice-container').appendChild(this.div);
+        this.div.addEventListener('click', rollDice);
+    }
+    
+    
+       
+    
+    this.roll = function () {
+        var num = Math.floor(Math.random() * 6) + 1;
+        this.value = num;
+        this.div.innerHTML = num;
+    }   
+    
+    this.sum = function() {
+        
+    }
+            
+ }
+function addDice() {
+    var die = new Die();
+    die.insert();
+    loadedDie.push(die);
+}
+
+var loadedDie = [];
+
+function rollDice() {
+    for (var i = 0; i < loadedDie.length; i++) {
+        loadedDie[i].roll();    
+    }   
+    
+}
+
+// Extra credit
+
+
+
+
